8af57c814537aed22023e57ed7db6654d65e4ae0
Yanzeyi <115777770+Yanzeyi@users.noreply.github.com>
2023-04-06 00:31:18 +0800

    Add files via upload

---
+++ data_clean.py
@@ -0,0 +1,157 @@
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import numpy as np
+from error_detect import k_means, three_sigma, iostation_forest, box_plot
+# from sklearn.ensemble import IsolationForest
+
+
+class ds_load():
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.ds = pd.read_csv(self.file_path, encoding='ANSI', parse_dates=['MDATE'], date_parser=lambda x : datetime.strptime(x, "%Y-%m-%d %H:%M:%S"))
+        
+        self.rows = len(self.ds)
+        self.cols = len(self.ds.columns)
+
+        self.SLData_dict = {}
+
+
+    def sort_by_time_order(self):
+        """
+        根据时间顺序进行排序
+        """
+        self.ds = self.ds.sort_values(by='MDATE')
+        self.ds = self.ds.reset_index(drop=True)
+
+    
+    def get_SLdata_index(self, sensor_id):
+        """
+        arg:sensor_id type str
+        return: index_list 某一个传感器的全部数据的索引列表
+        """
+        sensor_id_df = self.ds[['SENSOR_ID']]
+        index_list = []
+        for i in range(self.rows):
+            if str(sensor_id_df.iloc[i, 0]) == sensor_id:
+                index_list.append(i)
+        return index_list
+
+
+    def get_SLdata(self, index_list):
+        """
+        arg:index_list type list 索引
+        return: SLdata_list type list 根据索引值得到数据列表
+        """
+        SLdata_list = []
+        for index in index_list:
+            SLdata_list.append(self.ds.iloc[index, 11])
+        return SLdata_list
+    
+
+    def show_pic(self, SLdata_list):
+        """
+        arg:SLdata_list type list 数据列表
+        根据数据列表生成折线图
+        """
+        x = range(len(SLdata_list))
+        plt.plot(x, SLdata_list)
+        plt.show()
+
+    
+    def df2dict_SLData(self, country_id):
+        """
+        arg:country_id type bool China 0 Russsia 1
+        将索力数据转换成字典形式
+        example: {"SLS01": [1, 3, 5, 6 ,7], "SLS02":[2, 5, 7 ,8], ...}
+        """
+        SENSOR_PRIFIX = "SLS" if country_id == 0 else "SLX"
+        for i in range(1, 25, 1):
+            if i < 10 : sensor_id = SENSOR_PRIFIX + "0" + str(i)
+            elif 9 < i < 25 : sensor_id = SENSOR_PRIFIX + str(i)
+            print(sensor_id)
+            index_list = self.get_SLdata_index(sensor_id)
+            SLdata_list = self.get_SLdata(index_list)
+            self.SLData_dict[str(sensor_id)] = SLdata_list
+
+
+    def detect_error_pic(self, SLdata_list, error_index_list):
+        """
+        arg:SLdata_list type list 数据列表
+            error_index_list type list 异常值索引列表
+        将数据画成折线图，并将异常值标成红点
+        """
+        x = range(len(SLdata_list))
+        plt.plot(x, SLdata_list)
+        for i in range(len(error_index_list)):
+            plt.scatter(error_index_list[i], SLdata_list[error_index_list[i]], c = 'r')
+        plt.show()
+
+
+if __name__ == "__main__":
+
+    file_path =  r"D:\Jilin_university\Harbin_bridge_pro\原始数据-按月\原始数据-按月\2022-01.csv"  #导入数据库路径
+
+
+    # ds = pd.read_csv(file_path, encoding='ANSI', parse_dates=['MDATE'], date_parser=lambda x : datetime.strptime(x, "%Y-%m-%d %H:%M:%S"))
+    # ds = ds.sort_values(by='MDATE')
+
+    dl = ds_load(file_path) #加载数据库
+
+
+    dl.sort_by_time_order() #按时间进行排序
+    dl.df2dict_SLData(0)
+    print(dl.SLData_dict)
+    # index_list = dl.get_SLdata_index("SLS03") #查找传感器数据的索引
+    # SLdata_list = dl.get_SLdata(index_list) #根据索引值得到数据
+
+
+    # dl.df2dict_SLData()
+    # print(dl.SLData_dict)
+    # dl.show_pic(SLdata_list)
+
+    # detect_test = detect_error()
+    # error_list = detect_test.three_sigma(SLdata_list)
+    # print(error_list)
+
+    ################################### kmeans检测################################### 
+    # km = k_means()  #kmeans检测
+    # error_index_list = km.k_means_train(2, SLdata_list) #得到异常值索引
+    # dl.detect_error_pic(SLdata_list, error_index_list)  #作图(异常值标红)
+
+    ################################### 3sigma检测################################### 
+    # ts = three_sigma() #3sigma检测
+    # error_index_list_0 = ts.three_sigma_(SLdata_list)
+    # print(error_index_list_0)
+    # # dl.detect_error_pic(SLdata_list, error_index_list_0)
+
+    ################################### isolation forest检测################################### 
+    # # print(len(SLdata_list))
+    # isofor = iostation_forest()
+    # error_index_list = isofor.ioslation_forest_train(SLdata_list)
+    # dl.detect_error_pic(SLdata_list, error_index_list)
+
+    ################################### box_plot检测################################### 
+    # bp = box_plot()
+    # error_index_list1 = bp.box_plot_train(SLdata_list)
+    # # print(error_index_list1)
+    # dl.detect_error_pic(SLdata_list, error_index_list1)
+
+    # # x = range(len(SLdata_list))
+    # # plt.plot(x, SLdata_list)
+    # # for i in range(len(error_index_list)):
+    # #     plt.scatter(error_index_list[i], SLdata_list[error_index_list[i]], c = 'r')
+    # # plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ error_detect.py
@@ -0,0 +1,130 @@
+import numpy as np
+# from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.ensemble import IsolationForest
+from math import ceil, floor
+
+
+class k_means():    
+    def k_choose(self ,data_list):
+        SSE = []
+        for k in range(1,9):
+            k_tmp = KMeans(n_clusters=k)
+            k_tmp.fit(np.array(data_list).reshape(-1, 1))
+            SSE.append(k_tmp.inertia_) 
+        return SSE
+    
+
+    def k_means_train(self, k, data_list):
+        label_cnt = []
+        error_index = []
+        K_means_ = KMeans(n_clusters=k)
+        K_means_.fit(np.array(data_list).reshape(-1, 1))
+        # print(K_means_.labels_)
+        data_labels = list(K_means_.labels_)
+        # for i in range(len(data_labels)):
+        #     if data_labels[i] == 1:
+        #         print(i)
+        for i in range(k):
+            label_cnt.append(0)
+        for i in range(len(data_labels)):
+            label_cnt[data_labels[i]] = label_cnt[data_labels[i]] + 1
+        # print(label_cnt)
+        error_label = label_cnt.index(min(label_cnt))
+        # print(error_label)
+        for i in range(len(data_labels)):
+            if data_labels[i] == error_label: error_index.append(i)
+        return error_index
+
+
+class three_sigma():
+    def __init__(self):
+        pass
+
+    def three_sigma_(self, SLdata_list):
+        print(len(SLdata_list))
+        if np.std(SLdata_list) > np.mean(SLdata_list)/10:
+            error_index = []
+            for i in range(len(SLdata_list)):
+                if abs(SLdata_list[i] - np.mean(SLdata_list)) > 1.5 * np.std(SLdata_list):
+                    error_index.append(i)
+        else:
+            error_index = []
+            for i in range(len(SLdata_list)):
+                if abs(SLdata_list[i] - np.mean(SLdata_list)) > 3 * np.std(SLdata_list):
+                    error_index.append(i)
+
+        return error_index
+    
+
+def return_index(func):
+    def wrapper(*args, **kwargs):
+        label_ = func(*args, **kwargs)
+        error_index = []
+        for i in range(len(label_)):
+            if label_[i] == -1 : error_index.append(i)
+        return error_index
+    return wrapper
+
+
+def return_lrweights(func):
+    def wrapper(self, number):
+        close_num = func(self, number)
+        rn = close_num if close_num > number else close_num + 1
+        rw = 1 - rn + number
+        lw = 1 - rw
+        return lw, rw
+    return wrapper
+
+
+class iostation_forest():
+    @return_index
+    def ioslation_forest_train(self, SLdata_list):
+        isof = IsolationForest(n_estimators=50, max_samples='auto', contamination=float(0.005),max_features=1.0)
+        isof.fit(np.array(SLdata_list).reshape(-1, 1))
+        SLdata_label = isof.predict(np.array(SLdata_list).reshape(-1, 1))
+        return SLdata_label
+
+
+class box_plot():
+    def box_plot_train(self, data_list):
+        error_index_list = []
+        sorted_list = sorted(data_list)
+        num = len(sorted_list)
+        q1_pos = (num + 1) / 4 - 1
+        q2_pos = (num + 1) / 2 - 1
+        q3_pos = 3 * (num + 1)/ 4 - 1
+        q1 = self.get_q(q1_pos, sorted_list)
+        q2 = self.get_q(q2_pos ,sorted_list)
+        q3 = self.get_q(q3_pos ,sorted_list)
+        # print(q1, q2, q3)
+        IQR = q3 - q1
+        bottom_line = q1 - 1.5 * IQR
+        top_line = q3 + 1.5 * IQR
+        for data in data_list:
+            if data < bottom_line or data > top_line : error_index_list.append(data_list.index(data))
+        return error_index_list
+    
+
+    def get_q(self, q_pos, data_list):
+
+        q_lw, q_rw = self.left_or_right(q_pos)
+        return q_lw * data_list[floor(q_pos)] + q_rw * data_list[ceil(q_pos)]
+
+
+    @return_lrweights
+    def left_or_right(self, number):
+        left_diff = abs(number - ceil(number))
+        right_diff = abs(floor(number) - number)
+        return ceil(number) if left_diff <= right_diff else floor(number)
+
+
+if __name__ == "__main__":
+    pass
+    # data_list = [13, 13.5, 13.8, 13.9, 14, 14.6, 14.8, 15, 15.2, 15.4]
+    # b = box_plot()
+    # b.box_plot_train(data_list)
+
+    def hiahia():
+        pass
+    print(hiahia.__name__)
